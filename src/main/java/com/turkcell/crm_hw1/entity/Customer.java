c65713f3f6f48d197d98d3e3b16cf68438c7d502
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2025-01-11 12:40:22 +0300

    Customer created

--- src/main/java/com/turkcell/crm_hw1/entity/Customer.java
+++ src/main/java/com/turkcell/crm_hw1/entity/Customer.java
@@ -1,4 +1,29 @@
 package com.turkcell.crm_hw1.entity;
 
-public class Customer {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="customers")
+public class Customer extends BaseEntity{
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="contact")
+    private String contact;
+
+    @Column(name="identity_no")
+    private String identityNo;
+
+    @ManyToOne()
+    @JoinColumn(name="type_id")
+    private CustomerType type;
+
+    @ManyToOne()
+    @JoinColumn(name="segment_id")
+    private Segment segment;
 }
