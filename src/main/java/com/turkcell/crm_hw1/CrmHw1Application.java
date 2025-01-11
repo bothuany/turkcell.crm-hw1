440389c5cc03ca3b9dc7a48910926f4897014cec
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2025-01-11 12:15:44 +0300

    first commit

---
+++ src/main/java/com/turkcell/crm_hw1/CrmHw1Application.java
@@ -0,0 +1,13 @@
+package com.turkcell.crm_hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmHw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmHw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/turkcell/crm_hw1/CrmHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.crm_hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
