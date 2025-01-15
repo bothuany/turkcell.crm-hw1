8fa30fd82fc905c4e91b1d40bde00854c4bc63c8
ademvnc <ademmavanaci@gmail.com>
2025-01-15 22:00:36 +0300

    dtos
    Done

--- src/main/java/com/turkcell/crm_hw1/dtos/customer/CreateCustomerDto.java
+++ src/main/java/com/turkcell/crm_hw1/dtos/customer/CreateCustomerDto.java
@@ -1,11 +1,33 @@
 package com.turkcell.crm_hw1.dtos.customer;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.turkcell.crm_hw1.entity.CustomerType;
+import com.turkcell.crm_hw1.entity.Segment;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class CreateCustomerDto {
+
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String contact;
+
+    private String identityNo;
+
+    private CustomerType type;
+
+    private Segment segment;
+
 }
--- src/main/java/com/turkcell/crm_hw1/dtos/customer/UpdateCustomerDto.java
+++ src/main/java/com/turkcell/crm_hw1/dtos/customer/UpdateCustomerDto.java
@@ -1,11 +1,32 @@
 package com.turkcell.crm_hw1.dtos.customer;
 
+
+import com.turkcell.crm_hw1.entity.CustomerType;
+import com.turkcell.crm_hw1.entity.Segment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+
 public class UpdateCustomerDto {
+
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String contact;
+
+    private String identityNo;
+
+    private CustomerType type;
+
+    private Segment segment;
 }
